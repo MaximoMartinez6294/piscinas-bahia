b7df4d640afc5cf1a19c781911f6f321aed6a88a
MaximoMartinez6294 <122505523+MaximoMartinez6294@users.noreply.github.com>
2023-03-14 08:52:29 -0300

    agregue servicio

--- src/components/VarGalery.js
+++ src/components/VarGalery.js
@@ -5,7 +5,7 @@ const VarGalery = () => {
         <section className="contactor2" id="contactor2">
             <div className="contenido-var2">
                 <h1 className="titulo2">La mejor experiencia para alcanzar tu sueño.</h1>
-                <a href="/galeria" className="btn">Ver Galeria</a>
+                <a href="/servicio" className="btn">Ver Galeria</a>
             </div>
         </section>
 
